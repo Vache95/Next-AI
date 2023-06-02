2de35047d5a79e00a7c29ee8fe3b6e30ed3d4067
Vache95 <vachevirabyan1995@gmail.com>
2023-06-02 19:10:36 +0400

    feature/nav and next-auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 import '@style/globals.css'
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+
 export const metadata = {
   title: 'Promptopia',
   description: 'Discover & Share AI Prompts',
@@ -12,12 +15,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+       <Provider>
         <div className="main">
           <div className="gradient"/>
         </div>
         <main className="app">
+          <Nav/>
           {children}
         </main>
+        </Provider>
       </body>
     </html>
   )
